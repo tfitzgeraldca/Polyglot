09080004cb97c168ba19a9682dc0c49f436fee7c
Tim FitzGerald <tim.fitzgerald@edelman.com>
2013-11-28 00:25:21 -0500

    Update translations to add-on.

--- system/expressionengine/third_party/language/english/lang.polyglot.php
+++ system/expressionengine/third_party/language/english/lang.polyglot.php
@@ -5,5 +5,9 @@ $lang = array(
   'language_pattern' => 'Language Pattern:',                      
   'sd' => 'Subdomain (xx.domain.com)',
   'segment' => 'First segment (domain.com/xx/)',
-  'default_language' => 'Default Language:',      
+  'none' => 'None',
+  'default_language' => 'Default Language:',
+  'cldr_json_path' => 'Path to CLDR JSON:',
+  'language_path' => 'Path to Glossary (_lang.php) Files:',
+  'variable_prefix' => 'Early-Parsed Variable Prefix:'
 );
---
+++ system/expressionengine/third_party/language/french/lang.polyglot.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang = array(
+  'language_pattern' => 'Méthode de détection de la langue :',                      
+  'sd' => 'Sous-domaine (xx.domaine.com)',
+  'segment' => 'Premier segment (domaine.com/xx/)',
+  'none' => 'Aucune',
+  'default_language' => 'Langue par défaut :',
+  'cldr_json_path' => 'Parcours vers le JSON CLDR :',
+  'language_path' => 'Parcours vers les fichiers de glossaire (_lang.php) :',
+  'variable_prefix' => 'Préfixe pour les variables interprétées :'
+);
